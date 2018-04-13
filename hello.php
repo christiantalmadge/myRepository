7ef34b94e817588f2058c2284b6f102b9cb34822
Christian Talmadge <ctalmadge2@mail.greenriver.edu>
2018-04-12 19:39:07 -0700

    Added code in hello.php to make it HTML5 valid

--- hello.php
+++ hello.php
@@ -1,3 +1,12 @@
-<?php
-	echo 'Hello, World!';
-?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP Hello</title>
+</head>
+<body>
+	
+	<?php
+	echo '<p>Hello, world!</p>';
+	?>
+</body>
+</html>
